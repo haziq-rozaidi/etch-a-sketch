5ea432a63522294dea96781a4a93da1575bb84ba
Haziq Rozaidi <haziq050946@gmail.com>
2023-02-26 15:06:50 +0800

    Add coloring functionalities and modes

--- scripts/index.js
+++ scripts/index.js
@@ -1,14 +1,26 @@
 const grid = document.querySelector('.grid');
 const range = document.querySelector('.settings__range');
 const label = document.querySelector('.settings__slider label');
-const btnApply = document.querySelector('#btnApply');
+const inputColor = document.querySelector('.settings__color');
+const btnNormal = document.querySelector('#btnNormal');
+const btnRainbow = document.querySelector('#btnRainbow');
+const btnEraser = document.querySelector('#btnEraser');
+const btnReset = document.querySelector('#btnReset');
 
 let defaultSize = Number(range.defaultValue);
+let isNormal = false;
+let isRainbow = false;
+let isEraser = false;
 
+// Initialise grid with grid items
 label.textContent = defaultSize + ' x ' + defaultSize;
 grid.style.gridTemplateColumns = `repeat(${defaultSize}, 1fr)`;
 grid.style.gridTemplateRows = `repeat(${defaultSize}, 1fr)`;
 createGridItems(defaultSize);
+
+// Activate normal mode
+btnNormal.classList.add('btn_active');
+isNormal = true;
 
 function createGridItems(size) {
     const gridArea = Math.pow(size, 2);
@@ -20,7 +32,60 @@ function createGridItems(size) {
     }
 }
 
-btnApply.addEventListener('click', () => {
+function getRandomInt(max) {
+    return Math.floor(Math.random() * (max + 1));
+}
+
+function getRandomColor() {
+    const color = `rgb(${getRandomInt(255)}, ${getRandomInt(255)}, ${getRandomInt(255)})`;
+    return color;
+}
+
+function changeBackgroundColor(gridItem, isNormal, isRainbow, isEraser) {
+    if (isNormal) {
+        const color = inputColor.value;
+        gridItem.style.backgroundColor = color;
+    }
+    else if (isRainbow) {
+        const color = getRandomColor();
+        gridItem.style.backgroundColor = color;
+    }
+    else if (isEraser) {
+        const color = '#FFF';
+        gridItem.style.backgroundColor = color;
+    }
+}
+
+function handleClick(e) {
+    const btn = e.target;
+
+    btnNormal.classList.remove('btn_active');
+    btnRainbow.classList.remove('btn_active');
+    btnEraser.classList.remove('btn_active');
+
+    isNormal = isRainbow = isEraser = false;
+
+    if (btn === btnNormal) {
+        btnNormal.classList.add('btn_active');
+        isNormal = true;
+    }
+    else if (btn === btnRainbow) {
+        btnRainbow.classList.add('btn_active');
+        isRainbow = true;
+    }
+    else if (btn === btnEraser) {
+        btnEraser.classList.add('btn_active');
+        isEraser = true;
+    }
+}
+
+btnNormal.addEventListener('click', handleClick);
+
+btnRainbow.addEventListener('click', handleClick);
+
+btnEraser.addEventListener('click', handleClick);
+
+btnReset.addEventListener('click', () => {
     const size = range.value;
     grid.replaceChildren();
     grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
@@ -31,4 +96,16 @@ btnApply.addEventListener('click', () => {
 range.addEventListener('input', (e) => {
     const value = e.target.value;
     label.textContent = value + ' x ' + value;
+});
+
+grid.addEventListener('mousedown', (e) => {
+    const gridItem = e.target;
+    changeBackgroundColor(gridItem, isNormal, isRainbow, isEraser);
+})
+
+grid.addEventListener('mouseover', (e) => {
+    const gridItem = e.target;
+    if (e.buttons === 1) {
+        changeBackgroundColor(gridItem, isNormal, isRainbow, isEraser);
+    }
 });
